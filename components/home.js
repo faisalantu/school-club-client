e0cee550219d3e72dd9427c2c74f9cad724a5961
faisalantu <faisalantu@gmail.com>
2021-03-21 02:06:59 +0600

    💄 changed few ui

--- components/home.js
+++ components/home.js
@@ -1,19 +1,18 @@
 import Carousel from "./home/carousel";
 import ClubCard from "./home/clubCard";
 export default function home() {
-
-    return (<div>
-        <div className="lg:text-2xl">Home</div>
-        <Carousel />
-        <div className="grid grid-cols-3">
-        <ClubCard  clubName="Computer Science & Engineering" />
-        <ClubCard  clubName="Computer Science & Engineering" />
-        <ClubCard  clubName="Electrical and Electronics Engineering"/>
-        <ClubCard  clubName="Computer Science & Engineering" />
-        <ClubCard  clubName="Computer Science & Engineering" />
-        <ClubCard  clubName="Electrical and Electronics Engineering" />
-        </div>
-
-    </div>)
-
+  return (
+    <div className='m-3'>
+      <div className='lg:text-2xl'>Home</div>
+      <Carousel />
+      <div className='grid grid-cols-3 gap-5 m-1'>
+        <ClubCard clubName='Computer Science & Engineering' />
+        <ClubCard clubName='Computer Science & Engineering' />
+        <ClubCard clubName='Electrical and Electronics Engineering' />
+        <ClubCard clubName='Computer Science & Engineering' />
+        <ClubCard clubName='Computer Science & Engineering' />
+        <ClubCard clubName='Electrical and Electronics Engineering' />
+      </div>
+    </div>
+  );
 }
