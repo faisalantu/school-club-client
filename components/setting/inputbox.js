96e2085e808b472c27f16a1e7a2af933123a7ee3
faisalantu <faisalantu@gmail.com>
2021-03-28 20:28:16 +0600

    🐛 BUG:  fixed few bugs > inputbox.js

--- components/setting/inputbox.js
+++ components/setting/inputbox.js
@@ -1,6 +1,18 @@
 export default function InputBox(props) {
-    return (<div className='flex flex-col mb-8 sm:pr-6 md:pr-10 lg:pr-40 pr-4'>
-                <label className='mb-2 text-lg text-grey-darkest' >{props.label}</label>
-                <input className='rounded-lg border bg-gray-300 py-2 px-3 text-grey-darkest' type={props.type} placeholder={props.placeholder} name={props.name} onChange={props.ChangeHandel} value={props.value} disabled={!props.editMode && "disabled"} />
-            </div>);
+  return (
+    <div className='flex flex-col mb-8 sm:pr-6 md:pr-10 lg:pr-40 pr-4'>
+      <label className='mb-2 text-lg text-grey-darkest'>
+        {props.label ? props.label : ""}
+      </label>
+      <input
+        className='rounded-lg border bg-gray-300 py-2 px-3 text-grey-darkest'
+        type={props.type?props.type:''}
+        placeholder={props.placeholder?props.placeholder:''}
+        name={props.name?props.name:''}
+        onChange={props.ChangeHandel?props.ChangeHandel:''}
+        value={props.value?props.value:''}
+        disabled={props.editMode?!props.editMode:'' && "disabled"}
+      />
+    </div>
+  );
 }
