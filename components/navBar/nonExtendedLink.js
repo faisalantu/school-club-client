e0884a6f248737799b74e1e8d981d18bd72104a4
faisalantu <faisalantu@gmail.com>
2021-03-28 18:00:37 +0600

    🍏 fixed few bugs and added a component

--- components/navBar/nonExtendedLink.js
+++ components/navBar/nonExtendedLink.js
@@ -1,11 +1,21 @@
-
-
-function nonExtendedLink() {
-    return (
-        <div>
-            
-        </div>
-    )
+import Link from "next/link";
+function nonExtendedLink({ name, iconClass, icon }) {
+  return (
+    <div className='mb-2'>
+      <Link href='#'>
+        <a className='hover:text-yellow-500 cursor-pointer inline-block'>
+          <div className='flex items-center'>
+            <div className='ml-0.5 mr-3 '>
+              <div className={`${iconClass ? iconClass : ""}`}>
+                {icon ? icon : ""}
+              </div>
+            </div>
+            <span>{name ? name : "Invalid Name"}</span>
+          </div>
+        </a>
+      </Link>
+    </div>
+  );
 }
 
-export default nonExtendedLink
+export default nonExtendedLink;
--- components/navBar/subSidebarLink.js
+++ components/navBar/subSidebarLink.js
@@ -4,8 +4,10 @@ function subSidebarLink({ icon, name, route }) {
   return (
     <div>
       <Link href={route ? route : "#"}>
-        <a className='flex items-center mt-1 hover:text-yellow-500 cursor-pointer'>
-          {icon ? icon : ""} <span className='ml-1'>{name ? name : ""}</span>
+        <a className='inline-block mt-1 hover:text-yellow-500 cursor-pointer'>
+          <div className='flex items-center'>
+            {icon ? icon : ""} <span className='ml-1'>{name ? name : ""}</span>
+          </div>
         </a>
       </Link>
     </div>
