031a1a84ff718e5c8d623511d52da5dedc693a59
faisalantu <faisalantu@gmail.com>
2021-03-29 02:32:47 +0600

    + added /admin/add-post file

---
+++ pages/admin/add-post.js
@@ -0,0 +1,23 @@
+import { connect } from "react-redux";
+import ContainerWrapper from "../../components/containerWrapper";
+import { wrapper } from "../../store/store";
+const setting = (props) => {
+  return (
+    <>
+      <ContainerWrapper>
+        <div className='w-11/12 mx-auto'>
+
+        </div>
+      </ContainerWrapper>
+    </>
+  );
+};
+
+// redux setup
+export const getStaticProps = wrapper.getStaticProps(async ({ store }) => {});
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+export default connect(null, mapDispatchToProps)(setting);
