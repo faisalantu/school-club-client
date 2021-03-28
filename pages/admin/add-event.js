68cf614ee54becfc369c05102cc58bf08570e9aa
faisalantu <faisalantu@gmail.com>
2021-03-28 20:17:12 +0600

    + page/admin/add-event

---
+++ pages/admin/add-event.js
@@ -0,0 +1,22 @@
+import { connect } from "react-redux";
+import ContainerWrapper from "../../components/containerWrapper";
+import UpdateAccuntInfo from "../../components/setting/updateAccuntInfo";
+import { wrapper } from "../../store/store";
+const setting = (props) => {
+  return (
+    <>
+      <ContainerWrapper>
+        
+      </ContainerWrapper>
+    </>
+  );
+};
+
+// redux setup
+export const getStaticProps = wrapper.getStaticProps(async ({ store }) => { });
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+export default connect(null, mapDispatchToProps)(setting);
