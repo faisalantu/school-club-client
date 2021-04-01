ead870eee990d79a3f9d33f317a55b2ca912777a
faisalantu <faisalantu@gmail.com>
2021-04-01 17:06:16 +0600

    🆕 NEW:  Dynamic route /admin/edit-member/abx

--- pages/admin/edit-member/[pid].js
+++ pages/admin/edit-member/[pid].js
@@ -1,10 +1,36 @@
-import { useRouter } from 'next/router'
+import { useState } from "react";
+import { useRouter } from "next/router";
+import ContainerWrapper from "../../../components/containerWrapper";
+import CheckBox from "../../../components/setting/checkBox";
+import Image from "next/image";
+const SelectMember = () => {
 
-const Post = () => {
-  const router = useRouter()
-  const { pid } = router.query
+  const router = useRouter();
 
-  return <p>Post: {pid}</p>
-}
+  return (
+    <div>
+      <ContainerWrapper>
+        <div>
+          <div className=' flex flex-row justify-start items-center mt-2'>
+            <div className='bg-white rounded-full w-44 h-44 relative overflow-hidden'>
+              <Image
+                src='/portrait-1.jpg'
+                layout='fill'
+                objectFit='cover'
+              ></Image>
+            </div>
+            <div className=' ml-10 '>
+              <h1 className=' font-semibold text-4xl py-2'>Faisal Antu</h1>
+              <span className='text-xl '>ID: 163 XXX XXX</span>
+            </div>
+          </div>
+          <div>
 
-export default Post
+          </div>
+        </div>
+      </ContainerWrapper>
+    </div>
+  );
+};
+
+export default SelectMember;
