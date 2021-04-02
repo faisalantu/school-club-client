8e92fdd873a0d7df48dfdba2ac019089663d95f9
Rashed Afridi <rashedafridi@gmail.com>
2021-04-02 15:31:00 +0600

    fix

--- pages/admin/add-event.js
+++ pages/admin/add-event.js
@@ -1,6 +1,6 @@
 import { connect } from "react-redux";
 import ContainerWrapper from "../../components/containerWrapper";
-import InputBox from "../../components/setting/inputbox";
+import InputBox from "../../components/global/inputbox";
 import { wrapper } from "../../store/store";
 const setting = (props) => {
   return (
