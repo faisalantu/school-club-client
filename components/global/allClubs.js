6a8ddb0488707e55bc903c47b3aa9834951e187d
faisalantu <faisalantu@gmail.com>
2021-04-19 02:54:03 +0600

    added route props

--- components/global/allClubs.js
+++ components/global/allClubs.js
@@ -1,14 +1,14 @@
 import ClubCard from "./clubsCard";
-export default function home() {
+export default function home({route}) {
   return (
     <div className='m-3'>
       <div className='grid grid-cols-3 gap-5 m-1'>
-        <ClubCard route='clubinfo/cse' clubName='Computer Science & Engineering' />
-        <ClubCard route='clubinfo/cse' clubName='Electrical and Electronics Engineering' />
-        <ClubCard route='clubinfo/cse' clubName='Computer Science & Engineering' />
-        <ClubCard route='clubinfo/cse' clubName='Computer Science & Engineering' />
-        <ClubCard route='clubinfo/cse' clubName='Computer Science & Engineering' />
-        <ClubCard route='clubinfo/cse' clubName='Electrical and Electronics Engineering' />
+        <ClubCard route={route?route:'clubinfo/cse'} clubName='Computer Science & Engineering' />
+        <ClubCard route={route?route:'clubinfo/cse'} clubName='Electrical and Electronics Engineering' />
+        <ClubCard route={route?route:'clubinfo/cse'} clubName='Computer Science & Engineering' />
+        <ClubCard route={route?route:'clubinfo/cse'} clubName='Computer Science & Engineering' />
+        <ClubCard route={route?route:'clubinfo/cse'} clubName='Computer Science & Engineering' />
+        <ClubCard route={route?route:'clubinfo/cse'} clubName='Electrical and Electronics Engineering' />
       </div>
     </div>
   );
