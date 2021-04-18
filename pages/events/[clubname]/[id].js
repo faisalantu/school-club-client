22cf85f5ec001e9a0047f2faf2b9b4521d70997c
faisalantu <faisalantu@gmail.com>
2021-04-19 04:41:45 +0600

    added roundedUser component

--- pages/events/[clubname]/[id].js
+++ pages/events/[clubname]/[id].js
@@ -1,4 +1,4 @@
-import Tags from "../../../components/global/tags";
+import { useState } from "react";
 import UserInfoAndDate from "../../../components/global/userInfoAndDate";
 import Comments from "../../../components/postComponent/comments";
 import Reaction from "../../../components/postComponent/reaction";
@@ -6,37 +6,156 @@ import CommentForm from "../../../components/postComponent/commentForm";
 import PostBody from "../../../components/postComponent/postBody";
 import ContainerWrapper from "../../../components/containerWrapper";
 import Image from "next/image";
-const Post = ({ imgUrl }) => {
+
+import { ImLocation } from "react-icons/im";
+import { RiMoneyDollarCircleFill } from "react-icons/ri";
+import { GiTicket, GiHand } from "react-icons/gi";
+import { BiTimeFive, BiMailSend, BiPhone, BiPlus } from "react-icons/bi";
+import RoundedUserList from "../../../components/global/roundedUserList";
+const Post = () => {
+  const [eventData, setEventData] = useState({
+    title: "10 Trending projects on GitHub for web developer",
+    dateCreated: "3 Feb 2021",
+    location: "Dhaka, Bangladesh",
+    fee: "100",
+    tickets: "34",
+    time: "4:00pm - 5:30pm",
+    email: "hello@faisalantu.com",
+    phone: "88017 XXX XXXX",
+    imgUrl: "/event-banner.webp",
+  });
+  const [UserData, setUserData] = useState({
+    name: "Faisal Antu",
+    role: "CSE Club President",
+    imgUrl: "/portrait-2.jpeg",
+  });
   return (
     <>
       <ContainerWrapper>
         <div className='py-2 mx-5'>
-          <div className=' border-b-2 pb-4'>
+          <div className=' border-b pb-4'>
             <h1 className=' text-3xl font-medium'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
-              facilis ea tempore.
+              {eventData.title && eventData.title}
             </h1>
-            <Tags tags={["git", "gitHub", "nodeJs", "nextJs"]} />
             <div className='mt-3'>
               <UserInfoAndDate
-                name='Faisal Antu'
-                role='CSE Club President'
-                imgUrl='/portrait-2.jpeg'
-                date='3 Feb 2021'
+                name={UserData.name}
+                role={UserData.role}
+                imgUrl={UserData.imgUrl ? UserData.imgUrl : "/user.png"}
+                date={eventData.dateCreated}
               />
             </div>
-            <div className=' w-full '>
-              <Image
-                src={imgUrl ? imgUrl : "/event-banner.webp"}
-                layout='responsive'
-                objectFit='contain'
-                height={10}
-                width={16}
-                loading='lazy'
-                quality={50}
-              ></Image>
+
+            <div className='mt-3 leading-relaxed'>
+              <div className='flex flex-wrap'>
+                <div className='flex items-center mr-5'>
+                  <ImLocation className=' text-sm' />
+                  <h3 className='ml-1'>
+                    {eventData.location && eventData.location}
+                  </h3>
+                </div>
+                <div className='flex items-center mr-5'>
+                  <RiMoneyDollarCircleFill className=' text-sm' />
+                  <p className='ml-1'>
+                    Fee {eventData.fee && eventData.fee} taka
+                  </p>
+                </div>
+                <div className='flex items-center mr-5'>
+                  <GiTicket className='text-sm' />
+                  <p className='ml-1'>
+                    {eventData.tickets && eventData.tickets} tickets left
+                  </p>
+                </div>
+                <div className='flex items-center mr-5'>
+                  <BiTimeFive className=' text-sm' />
+                  <p className='ml-1'>
+                    Time: {eventData.time && eventData.time}
+                  </p>
+                </div>
+                <div className='flex items-center mr-5'>
+                  <BiMailSend className=' text-sm' />
+                  <p className='ml-1'>{eventData.email && eventData.email}</p>
+                </div>
+                <div className='flex items-center mr-5'>
+                  <BiPhone className=' text-sm' />
+                  <p className='ml-1'>{eventData.phone && eventData.phone}</p>
+                </div>
+              </div>
             </div>
+
+            {eventData.imgUrl && (
+              <div className=' w-full '>
+                <Image
+                  src={
+                    eventData.imgUrl ? eventData.imgUrl : "/event-banner.webp"
+                  }
+                  layout='responsive'
+                  objectFit='contain'
+                  height={10}
+                  width={16}
+                  loading='lazy'
+                  quality={50}
+                ></Image>
+              </div>
+            )}
             <PostBody />
+            <div>
+              <h1 className='text-2xl font-medium mt-5 mb-3'>
+                Volunteers{" "}
+                <span className='bg-gray-300 rounded-full px-3 py-0.5 text-base font-normal ml-1'>
+                  12
+                </span>
+              </h1>
+              <div className='flex gap-3 flex-wrap'>
+                <RoundedUserList name='Faisal Antu' imgUrl='/portrait-1.jpg' />
+                <RoundedUserList name='Faisal Antu' imgUrl='/portrait-2.jpeg' />
+                <RoundedUserList name='Faisal Antu' imgUrl='/portrait-2.jpeg' />
+                <RoundedUserList name='Faisal Antu' imgUrl='/portrait-1.jpg' />
+                <RoundedUserList name='Faisal Antu' imgUrl='/portrait-2.jpeg' />
+                <RoundedUserList name='Faisal Antu' imgUrl='/portrait-2.jpeg' />
+              </div>
+            </div>
+
+            <div>
+              <h1 className='text-2xl font-medium mt-5 mb-3'>
+                Participants
+                <span className='bg-gray-300 rounded-full px-3 py-0.5 text-base font-normal ml-1'>
+                  35
+                </span>
+              </h1>
+              <div className='flex gap-3 flex-wrap'>
+                <RoundedUserList name='Faisal Antu' imgUrl='/portrait-1.jpg' />
+                <RoundedUserList name='Faisal Antu' imgUrl='/portrait-2.jpeg' />
+                <RoundedUserList name='Faisal Antu' imgUrl='/portrait-2.jpeg' />
+                <RoundedUserList name='Faisal Antu' imgUrl='/portrait-1.jpg' />
+                <RoundedUserList name='Faisal Antu' imgUrl='/portrait-2.jpeg' />
+                <RoundedUserList name='Faisal Antu' imgUrl='/portrait-2.jpeg' />
+                <RoundedUserList name='Faisal Antu' imgUrl='/portrait-1.jpg' />
+                <RoundedUserList name='Faisal Antu' imgUrl='/portrait-2.jpeg' />
+                <RoundedUserList name='Faisal Antu' imgUrl='/portrait-2.jpeg' />
+                <RoundedUserList name='Faisal Antu' imgUrl='/portrait-1.jpg' />
+                <RoundedUserList name='Faisal Antu' imgUrl='/portrait-2.jpeg' />
+                <RoundedUserList name='Faisal Antu' imgUrl='/portrait-2.jpeg' />
+                <RoundedUserList name='Faisal Antu' imgUrl='/portrait-1.jpg' />
+                <RoundedUserList name='Faisal Antu' imgUrl='/portrait-2.jpeg' />
+                <RoundedUserList name='Faisal Antu' imgUrl='/portrait-2.jpeg' />
+                <RoundedUserList name='Faisal Antu' imgUrl='/portrait-1.jpg' />
+                <RoundedUserList name='Faisal Antu' imgUrl='/portrait-2.jpeg' />
+                <RoundedUserList name='Faisal Antu' imgUrl='/portrait-2.jpeg' />
+                <RoundedUserList name='Faisal Antu' imgUrl='/portrait-1.jpg' />
+                <RoundedUserList name='Faisal Antu' imgUrl='/portrait-2.jpeg' />
+                <RoundedUserList name='Faisal Antu' imgUrl='/portrait-2.jpeg' />
+                <RoundedUserList name='Faisal Antu' imgUrl='/portrait-1.jpg' />
+                <RoundedUserList name='Faisal Antu' imgUrl='/portrait-2.jpeg' />
+                <RoundedUserList name='Faisal Antu' imgUrl='/portrait-2.jpeg' />
+                <RoundedUserList name='Faisal Antu' imgUrl='/portrait-1.jpg' />
+                <RoundedUserList name='Faisal Antu' imgUrl='/portrait-2.jpeg' />
+                <RoundedUserList name='Faisal Antu' imgUrl='/portrait-2.jpeg' />
+                <RoundedUserList name='Faisal Antu' imgUrl='/portrait-1.jpg' />
+                <RoundedUserList name='Faisal Antu' imgUrl='/portrait-2.jpeg' />
+                <RoundedUserList name='Faisal Antu' imgUrl='/portrait-2.jpeg' />
+              </div>
+            </div>
             <Reaction reactions='12' className='mt-4' />
           </div>
           <div>
