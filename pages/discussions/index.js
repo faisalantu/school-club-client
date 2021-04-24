4d93e6abb5385cf8531d65407fb06017eba6a532
faisalantu <faisalantu@gmail.com>
2021-04-24 20:05:48 +0600

    created new routes

---
+++ pages/discussions/index.js
@@ -0,0 +1,16 @@
+import ContainerWrapper from "../../components/containerWrapper";
+import AllClubs from '../../components/global/allClubs'
+
+const Index = (props) => {
+  return (
+    <>
+      <ContainerWrapper>
+        <div className='flex flex-col gap-10 flex-wrap mb-5 px-5'>
+          <AllClubs route="/discussions/cse"/>
+        </div>
+      </ContainerWrapper>
+    </>
+  );
+};
+
+export default Index;
