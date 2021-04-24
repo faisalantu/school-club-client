e741c73118d5516446ebe83919df13a0ed19d517
faisalantu <faisalantu@gmail.com>
2021-04-24 20:04:43 +0600

    add post page for user

---
+++ components/global/addPost.js
@@ -0,0 +1,61 @@
+import InputBox from "./inputbox";
+import DraftEditor from "./textEditor";
+import TagInput from "./tagInput";
+import ImageInputBox from "./imageInputBox";
+const AddPost = (props) => {
+  //tag data from tag component
+  const getTags = (tags) => console.log(tags);
+  return (
+    <>
+      <div className='px-5'>
+        <h1 className='font-bold text-3xl mb-3'>Add Post</h1>
+        <div>
+          <div className='flex flex-col mb-8'>
+            <label className='mb-2 text-lg text-grey-darkest'>Add Title</label>
+            <input
+              className='rounded border-2 border-gray-300 bg-transparent py-2 px-3 text-grey-darkest outline-none'
+              placeholder='your title here'
+              type='text'
+            />
+          </div>
+          <div>
+            <label className='mb-2 text-lg text-grey-darkest'>
+              Select Image
+            </label>
+            <ImageInputBox />
+          </div>
+        </div>
+        <div className='mt-5'>
+          <label className='text-lg'>Write Something</label>
+          <div className='border-2 border-gray-300 rounded p-4'>
+            <DraftEditor />
+          </div>
+        </div>
+        <div className='mt-5'>
+          <label className='text-lg'>Input some tags</label>
+          <TagInput getTags={getTags} />
+        </div>
+        <div className=' mt-5'>
+          <input
+            type='checkbox'
+            id='vehicle1'
+            name='vehicle1'
+            value='Bike'
+          ></input>
+          <label htmlFor='vehicle1'> Publish</label>
+        </div>
+      </div>
+
+      <div className='flex flex-row justify-center my-3'>
+        <button
+          type='submit'
+          className='border-2 rounded-lg bg-gray-200 border-yellow-400 cursor-pointer px-5 py-2'
+        >
+          Save Change
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default AddPost;
