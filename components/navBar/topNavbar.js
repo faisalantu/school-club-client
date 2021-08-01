aae28463fa0709b352f162c3f045c8a898dbf7a3
faisalantu <faisalantu@gmail.com>
2021-08-01 13:55:19 +0600

    🚑 FIX: top navbar icon placement

--- components/navBar/topNavbar.js
+++ components/navBar/topNavbar.js
@@ -34,7 +34,7 @@ function topNavbar(props) {
     setAccountToggle(false);
   });
   return (
-    <div className=" w-full h-12 bg-gray-900 flex justify-between items-center whitespace-nowrap">
+    <div className=' w-full h-12 bg-gray-900 flex justify-between items-center whitespace-nowrap'>
       <div className={`  md:hidden flex items-center ml-4`}>
         <div
           onClick={() => {
@@ -53,15 +53,15 @@ function topNavbar(props) {
           />
         </div>
 
-        <h1 className="text-gray-100 font-semibold text-lg ml-2">LOGO</h1>
+        <h1 className='text-gray-100 font-semibold text-lg ml-2'>LOGO</h1>
       </div>
-      <div className="pl-4  ">
-        <h1 className="text-gray-100 font-semibold text-lg hidden md:block">
+      <div className='pl-4  '>
+        <h1 className='text-gray-100 font-semibold text-lg hidden md:block'>
           LOGO
         </h1>
       </div>
-      <div className="flex gap-2 text-2xl pr-4">
-        <BiSearch className="text-gray-100 text-md flex-auto mx-1" />
+      <div className='flex items-center gap-2 text-2xl pr-4'>
+        <BiSearch className='text-gray-100 text-md flex-auto mx-1' />
 
         {/* Add sector */}
         <TopNavAddBtn />
@@ -72,9 +72,9 @@ function topNavbar(props) {
           onClick={function () {
             setNotificationToggle(!notificationToggle);
           }}
-          className="relative cursor-pointer"
+          className='relative cursor-pointer'
         >
-          <MdNotifications className="text-gray-100 text-md flex-auto mx-1" />
+          <MdNotifications className='text-gray-100 text-md flex-auto mx-1' />
           <BsTriangleFill
             className={`${
               notificationToggle ? "" : "hidden"
@@ -85,20 +85,20 @@ function topNavbar(props) {
               notificationToggle ? "" : "hidden"
             } absolute right-0  mt-2 origin-top-right rounded-md shadow-lg  z-30`}
           >
-            <div className="px-2 py-2 bg-white rounded-md shadow dark-mode:bg-gray-700">
+            <div className='px-2 py-2 bg-white rounded-md shadow dark-mode:bg-gray-700'>
               <a
-                className="flex justify-start px-4 py-2 mt-2 font-semibold bg-transparent rounded-lg hover:bg-gray-200"
-                href="#"
+                className='flex justify-start px-4 py-2 mt-2 font-semibold bg-transparent rounded-lg hover:bg-gray-200'
+                href='#'
               >
-                <MdEvent className="p-0 mr-1.5 text-base" />
-                <span className="text-sm">New Event</span>
+                <MdEvent className='p-0 mr-1.5 text-base' />
+                <span className='text-sm'>New Event</span>
               </a>
               <a
-                className="flex justify-start px-4 py-2 mt-2 font-semibold bg-transparent rounded-lg hover:bg-gray-200"
-                href="#"
+                className='flex justify-start px-4 py-2 mt-2 font-semibold bg-transparent rounded-lg hover:bg-gray-200'
+                href='#'
               >
-                <RiLockPasswordLine className="p-0  mr-1.5 text-base" />
-                <span className="text-sm">Password Changed</span>
+                <RiLockPasswordLine className='p-0  mr-1.5 text-base' />
+                <span className='text-sm'>Password Changed</span>
               </a>
             </div>
           </div>
@@ -110,18 +110,20 @@ function topNavbar(props) {
           onClick={function () {
             setAccountToggle(!accountToggle);
           }}
-          className="relative cursor-pointer"
+          className='relative cursor-pointer'
         >
           {props.user.hasOwnProperty("imageObj") ? (
-            <Image
-              className=" rounded-full overflow-hidden flex-auto mx-1"
-              src={props.user.imageObj.url}
-              alt="user"
-              width={25}
-              height={25}
-            />
+            <div className=" flex items-center">
+              <Image
+                className=' rounded-full overflow-hidden mx-1'
+                src={props.user.imageObj.url}
+                alt='user'
+                width={23}
+                height={23}
+              />
+            </div>
           ) : (
-            <MdAccountCircle className="text-gray-100 text-md  mx-1" />
+            <MdAccountCircle className='text-gray-100 text-md  mx-1' />
           )}
           <BsTriangleFill
             className={`${
@@ -133,53 +135,53 @@ function topNavbar(props) {
               accountToggle ? "" : "hidden"
             } absolute right-0 mt-2 origin-top-right rounded-md shadow-lg z-30`}
           >
-            <div className="px-2 py-2 bg-white rounded-md shadow dark-mode:bg-gray-700">
+            <div className='px-2 py-2 bg-white rounded-md shadow '>
               {props.user.isAdmin || props.user.isPrecedent ? (
-                <Link href="/admin">
+                <Link href='/admin'>
                   <a
-                    className="flex justify-start px-4 py-2 mt-2 font-semibold bg-transparent rounded-lg hover:bg-gray-200"
-                    href="#"
+                    className='flex justify-start px-4 py-2 mt-2 font-semibold bg-transparent rounded-lg hover:bg-gray-200'
+                    href='#'
                   >
-                    <RiAdminFill className="p-0 mr-1.5 text-base" />
-                    <span className="text-sm">Admin</span>
+                    <RiAdminFill className='p-0 mr-1.5 text-base' />
+                    <span className='text-sm'>Admin</span>
                   </a>
                 </Link>
               ) : null}
 
-              <Link href="/dashboard">
+              <Link href='/dashboard'>
                 <a
-                  className="flex justify-start px-4 py-2 mt-2 font-semibold bg-transparent rounded-lg hover:bg-gray-200"
-                  href="#"
+                  className='flex justify-start px-4 py-2 mt-2 font-semibold bg-transparent rounded-lg hover:bg-gray-200'
+                  href='#'
                 >
-                  <RiDashboardLine className="p-0 mr-1.5 text-base" />
-                  <span className="text-sm">Dashboard</span>
+                  <RiDashboardLine className='p-0 mr-1.5 text-base' />
+                  <span className='text-sm'>Dashboard</span>
                 </a>
               </Link>
-              <Link href="/setting">
+              <Link href='/setting'>
                 <a
-                  className="flex justify-start px-4 py-2 mt-2 font-semibold bg-transparent rounded-lg hover:bg-gray-200"
-                  href="#"
+                  className='flex justify-start px-4 py-2 mt-2 font-semibold bg-transparent rounded-lg hover:bg-gray-200'
+                  href='#'
                 >
-                  <FiSettings className="p-0 mr-1.5 text-base" />
-                  <span className="text-sm">Settings</span>
+                  <FiSettings className='p-0 mr-1.5 text-base' />
+                  <span className='text-sm'>Settings</span>
                 </a>
               </Link>
-              <Link href="/setting/update-password">
+              <Link href='/setting/update-password'>
                 <a
-                  className="flex justify-start px-4 py-2 mt-2 font-semibold bg-transparent rounded-lg hover:bg-gray-200"
-                  href="#"
+                  className='flex justify-start px-4 py-2 mt-2 font-semibold bg-transparent rounded-lg hover:bg-gray-200'
+                  href='#'
                 >
-                  <BiLock className="p-0 mr-1.5 text-base" />
-                  <span className="text-sm">Change Password</span>
+                  <BiLock className='p-0 mr-1.5 text-base' />
+                  <span className='text-sm'>Change Password</span>
                 </a>
               </Link>
               <a
                 onClick={props.logout}
-                className="flex justify-start px-4 py-2 mt-2 font-semibold bg-transparent rounded-lg hover:bg-gray-200"
-                href="#"
+                className='flex justify-start px-4 py-2 mt-2 font-semibold bg-transparent rounded-lg hover:bg-gray-200'
+                href='#'
               >
-                <HiOutlineLogout className="p-0 mr-1.5 text-base" />
-                <span className="text-sm">Logout</span>
+                <HiOutlineLogout className='p-0 mr-1.5 text-base' />
+                <span className='text-sm'>Logout</span>
               </a>
             </div>
           </div>
